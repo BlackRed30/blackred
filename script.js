3cd8841ede01a0da3db1e8903a9132a2ce2026cb
BlackRed30 <37946199+BlackRed30@users.noreply.github.com>
2019-01-23 21:11:47 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -7,29 +7,59 @@
 // });
 
 // event pada saat link di klik
+
+// $('.tekan').on('click', function () {
+//     // ambil isi href
+//     var tujuan = $(this).attr('href');
+//     // tangkap elemen ybs
+//     var elemenTujuan = $(tujuan);
+
+//     //testing untuk nilai
+
+//     console.log(elemenTujuan.offset().top);
+
+// })
+
+//testing saat di scroll
+
+
+
 $(window).on('scroll', function () {
 
-    wScroll = $(this).scrollTop();
+    var wScroll = $(this).scrollTop();
 
-    if (wScroll == $('.introduction').offset().top) {
-        const ambil_introduction = document.querySelector('.animasi');
-        ambil_introduction.classList.add('animated', 'fadeInRight');
+    if (wScroll >= $('.introduction').offset().top) {
+        $('.animasi').addClass('animated fadeInLeft');
+    }
+    if (wScroll >= $('.done-so-far').offset().top) {
+        $('.animasi-2').addClass('animated fadeInLeft');
+    }
+    if (wScroll >= $('.portfolio').offset().top) {
+        $('.animasi-3').addClass('animated fadeInLeft');
+    }
+    if (wScroll >= $('.skill').offset().top) {
+        $('.animasi-4').addClass('animated fadeInLeft');
     }
 
-    //  pindahkan scroll
 
-    // $('window').scroll(function () {
-    //     var wScroll = $(this).scrollTop();
-
-    //     if (wScroll == elemenTujuan.offset().top) {
-
-    //     }
-    // });
-
+    // if (wScroll >= $('.done-so-far').offset().top) {
+    //     $('.animasi').addClass('animated fadeInLeft');
+    // }
+    // if (wScroll >= $('.portfolio').offset().top) {
+    //     $('.animasi').addClass('animated fadeInLeft');
+    // }
+    // if (wScroll >= $('.skill').offset().top) {
+    //     $('.animasi').addClass('animated fadeInLeft');
+    // }
+    // if (wScroll >= $('.call-me').offset().top) {
+    //     $('.animasi').addClass('animated fadeInLeft');
+    // }
 });
 
-//permasalahan disini inginnya saat digeser maka akan berjaan javascriptnya
-//namun disini saat digeser animasi tidak jalan saat di geser
-//padaha sebelumnya dapat dilakukan tanpa menggunakan if
-//namun kelemahan bila tanpa if javascript akan langsung berjalan
-//hanya di scroll sekali maka javascript langsung berjalan semua, tidak menunggu saat ada di halaman tertentu
+// disini permasalahan nya sudah hampir selesai namun terdapat
+// beberapa kesalahan
+// disini kesalahannya salah satunya adalah saat di scroll
+// animasi saat di start memang mengambil kondisi yang ditentukan
+// namun ternyata tidak apply terhadap 4 fungsi lain
+// dan mengikuti fungsi yang paling pertama
+// sehingga perlu diadakan analisis lebih jauh

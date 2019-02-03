7e5a9cc001af3ba079df3cf42335d788cbf18ec4
dityapratama <37946199+dityapratama@users.noreply.github.com>
2019-02-03 16:45:37 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,65 +1,17 @@
-// $(window).scroll(function () {
-// var wScroll = $(this).scrollTop();
-
-// const ambil_introduction = document.querySelector('.animasi_introduction')
-// ambil_introduction.classList.add('animated', 'fadeInRight')
-
-// });
-
-// event pada saat link di klik
-
-// $('.tekan').on('click', function () {
-//     // ambil isi href
-//     var tujuan = $(this).attr('href');
-//     // tangkap elemen ybs
-//     var elemenTujuan = $(tujuan);
-
-//     //testing untuk nilai
-
-//     console.log(elemenTujuan.offset().top);
-
-// })
-
-//testing saat di scroll
-
-
-
 $(window).on('scroll', function () {
 
     var wScroll = $(this).scrollTop();
 
-    if (wScroll >= $('.introduction').offset().top) {
+    if (wScroll >= $('.introduction').offset().top / 2) {
         $('.animasi').addClass('animated fadeInLeft');
     }
-    if (wScroll >= $('.done-so-far').offset().top) {
+    if (wScroll >= $('.done-so-far').offset().top / 1.5) {
         $('.animasi-2').addClass('animated fadeInLeft');
     }
-    if (wScroll >= $('.portfolio').offset().top) {
+    if (wScroll >= $('.portfolio').offset().top / 1.3) {
         $('.animasi-3').addClass('animated fadeInLeft');
     }
     if (wScroll >= $('.skill').offset().top) {
         $('.animasi-4').addClass('animated fadeInLeft');
     }
-
-
-    // if (wScroll >= $('.done-so-far').offset().top) {
-    //     $('.animasi').addClass('animated fadeInLeft');
-    // }
-    // if (wScroll >= $('.portfolio').offset().top) {
-    //     $('.animasi').addClass('animated fadeInLeft');
-    // }
-    // if (wScroll >= $('.skill').offset().top) {
-    //     $('.animasi').addClass('animated fadeInLeft');
-    // }
-    // if (wScroll >= $('.call-me').offset().top) {
-    //     $('.animasi').addClass('animated fadeInLeft');
-    // }
 });
-
-// disini permasalahan nya sudah hampir selesai namun terdapat
-// beberapa kesalahan
-// disini kesalahannya salah satunya adalah saat di scroll
-// animasi saat di start memang mengambil kondisi yang ditentukan
-// namun ternyata tidak apply terhadap 4 fungsi lain
-// dan mengikuti fungsi yang paling pertama
-// sehingga perlu diadakan analisis lebih jauh
